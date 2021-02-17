039066997fa47fa091dbee3e2e58d92cf683a76b
hippod01 <69491529+hippod01@users.noreply.github.com>
2021-02-17 14:04:46 +0900

    백준 1238(다익스트라)

---
+++ baekjoon/baekjoon_1238.cpp
@@ -0,0 +1,70 @@
+//https://www.acmicpc.net/problem/1238
+
+#include<iostream>
+#include<queue>
+#include<vector>
+#define INF 100000
+using namespace std;
+
+int n, m, x, u, v, w;
+vector<pair<int, int>>a1[1001];
+vector<pair<int, int>>a2[1001];
+int d[1001];
+int ans[1001];
+
+void dijkstra(int start, vector<pair<int, int>>arr[1001]){
+	
+	for (int i = 1; i <= n; i++){
+		d[i] = INF;
+    }
+
+	d[start] = 0;
+	priority_queue<pair<int, int>>pq;
+	pq.push({ 0,start });
+
+	while (!pq.empty()){
+		int current = pq.top().second;
+		int distance = -pq.top().first;
+
+		pq.pop();
+
+		if (d[current] < distance) continue;
+
+		for (int i = 0; i < arr[current].size(); i++){
+			int next = arr[current][i].first;
+			int next_distance = arr[current][i].second + distance;
+            
+			if (d[next] > next_distance){
+				d[next] = next_distance;
+				pq.push(make_pair(-next_distance, next));
+			}
+		}
+	}
+	
+	for (int i = 1; i <= n; i++)
+		ans[i] += d[i];
+}
+
+int main(){
+	cin.tie(0);
+	cin.sync_with_stdio(false);
+	cin >> n >> m >> x;
+	for (int i = 0; i < m; i++){
+		cin >> u >> v >> w;
+		//x에서 정방향 다익스트라
+		a1[u].push_back({ v,w });
+		//x에서 역방향 다익스트라
+		a2[v].push_back({ u,w });
+	}
+	dijkstra(x, a1);
+	dijkstra(x, a2);
+
+	int max = 0;
+	for (int i = 1; i <= n; i++){
+		if (max < ans[i])
+			max = ans[i];
+	}
+	cout << max << '\n';
+
+    return 0;
+}
