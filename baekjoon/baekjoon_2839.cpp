54d8329c84a82a70896a680ecbb08f182930c4a3
hippod01 <69491529+hippod01@users.noreply.github.com>
2021-02-24 15:42:11 +0900

    백준 2839(DP)

---
+++ baekjoon/baekjoon_2839.cpp
@@ -0,0 +1,44 @@
+//https://www.acmicpc.net/problem/2839
+
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int d[5001];
+
+void dp(int target){
+
+    if(target <  5 && target > 0){
+        if(target == 3) {
+            cout << "1" << '\n';
+        } else{
+            cout << "-1" << '\n';
+        } 
+    } else {
+        d[3] = d[5] = 1;
+        for(int i=6; i<=n; i++){
+            d[i] = 1+ min(d[i-3],d[i-5]);
+        }
+        if(d[target] >= 9999) {
+            cout << "-1" << '\n';
+        } else {
+            cout << d[target] << '\n';
+        }
+    }
+    
+    
+    
+}
+
+int main(void){
+
+    memset(d,9999,sizeof(d));
+
+    cin >> n;
+    dp(n);
+
+    return 0;
+}
