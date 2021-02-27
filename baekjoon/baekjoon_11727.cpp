3379e7ed2e94d3293e37d17b5531b14f76c19952
hippod01 <69491529+hippod01@users.noreply.github.com>
2021-02-27 10:35:56 +0900

    백준 11727(DP, 타일링)

--- baekjoon/baekjoon_11727.cpp
+++ baekjoon/baekjoon_11727.cpp
@@ -1,1 +1,25 @@
 //https://www.acmicpc.net/problem/11727
+
+#include <iostream>
+
+using namespace std;
+
+int d[1001];
+
+int dp(int x){
+    d[1] = 1;
+    d[2] = 3;
+    if(d[x] != 0) return d[x];
+    return d[x] = (dp(x-1) + (2*dp(x-2))) % 10007;
+
+}
+
+int main(void){
+
+    int n;
+    cin >> n;
+
+    cout << dp(n) << '\n';
+
+    return 0;
+}
